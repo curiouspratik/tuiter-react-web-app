c9bb17f88b97b0029408de5c00c74d5d46ba3827
Pratik Patil <patil.pratiq@gmail.com>
2023-03-27 14:07:14 -0400

    a7 html spring 2023

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,16 +1,18 @@
+import Nav from "../nav";
+import {Routes, Route} from "react-router"
 import Assignment6 from "./a6";
 import Assignment7 from "./a7";
-import Nav from "../nav";
-import {Route, Routes} from "react-router";
+
 function Labs() {
-  return(
-      <div>
-        <Nav/>
-        <Routes>
-          <Route index element={<Assignment6/>} />
-          <Route path="/a7" element={<Assignment7 />} />
-        </Routes>
-      </div>
-  );
+    return(
+        <div>
+            <Nav/>
+            {/*<h1>Assignment 6</h1>*/}
+            <Routes>
+                <Route index element={<Assignment6/>} />
+                <Route path="/a7" element={<Assignment7/>} />
+            </Routes>
+        </div>
+    );
 }
 export default Labs;
