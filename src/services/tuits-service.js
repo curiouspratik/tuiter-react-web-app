54352d1653437f579625a75f8ed73995071785b3
Pratik Patil <patil.pratiq@gmail.com>
2023-04-02 23:20:04 -0400

    a8 react spring 2023

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 // Auto switch API: Locally REACT_APP_API_BASE can be set to http://localhost:4000/api, but when the application runs remotely it can
 // instead be set to=
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = `${REACT_APP_API_BASE}/tuits`;
 
 // Declare four common CRUD operations.
 // The functions are all implemented as asynchronous functions that will not block the browser's sole JavaScript thread.
