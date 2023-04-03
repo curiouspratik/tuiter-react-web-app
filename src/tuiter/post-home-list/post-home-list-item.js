f16b1c35accc17b0a68650461aeb2d795e1cbd56
Pratik Patil <patil.pratiq@gmail.com>
2023-04-02 23:02:28 -0400

    a8 react spring 2023

--- src/tuiter/post-home-list/post-home-list-item.js
+++ src/tuiter/post-home-list/post-home-list-item.js
@@ -41,19 +41,24 @@ const PostHomeListItem = (
         <div className="list-group-item pt-3 wd-post-list-item border border-light">
             {post.retweeted &&
                 <div className="row align-items-center mb-1">
+                    {/* Left icon */}
                     <div className="col-1 px-0">
                         <FontAwesomeIcon icon="fa-solid fa-retweet" className="float-end" />
                     </div>
+                    {/* Right Person */}
                     <div className="col-11 ps-4 text-secondary fw-bold">
                         <span style={fontSize}>{post.retweeted} {" "} Retweeted</span>
                     </div>
                 </div>
             }
             <div className="row">
+                {/* Left Avatar */}
                 <div className="col-1">
                     <img src={`/images/${post.userAvatar}`} className="rounded-circle" width="48px" alt="avatar"/>
                 </div>
+                {/* Right Content */}
                 <div className="col-11 ps-4">
+                    {/* User's name handle and post time */}
                     <div className="row align-items-center">
                         <div className="col-11">
                             <span className="fw-bolder">{post.userName}{' '}</span>
@@ -64,10 +69,13 @@ const PostHomeListItem = (
                             <FontAwesomeIcon icon="fa-solid fa-ellipsis" className="text-secondary float-end"/>
                         </div>
                     </div>
+                    {/* Post Title */}
                     {post.markText && <MarkTextAsLink text={post.title} markText={post.markText}/>}
                     {!post.markText && post.title}
+                    {/* Inner Post */}
                     <InnerPost post={post.innerPost}
                                tweet={post.innerPost.type === "tweet" ? filterById(posts, post.innerPost.tweetId): null}/>
+                    {/* Bottom Icons */}
                     <div className="row mb-2">
                         <a href="/#" className="col text-secondary text-decoration-none">
                             <FontAwesomeIcon icon="fa-regular fa-comment" className="pe-2" />
@@ -85,6 +93,7 @@ const PostHomeListItem = (
                             <FontAwesomeIcon icon="fa-solid fa-arrow-up-from-bracket" className="pe-2" />
                         </a>
                     </div>
+                    {/*  If retweeted, show this thread  */}
                     {post.retweeted &&
                         <div className="my-3">
                             <a href="/#" className="text-decoration-none text-primary">Show this thread</a>
